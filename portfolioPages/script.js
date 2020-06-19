6d89e23cd39b2735abd79d66eb4a756908957fa1
Marri Stephen <stephenmarri7@gmail.com>
2020-06-19 15:39:55 +0530

    made most changes

---
+++ portfolioPages/script.js
@@ -0,0 +1,31 @@
+let projectTiles = document.querySelectorAll('.project-tile')
+
+projectTiles.forEach(x => {
+    x.addEventListener('click', tileHandler)
+})
+
+function tileHandler() {
+    switch (this.id) {
+        case 'cipher':
+            window.open("https://stephenmarri.github.io/js_projects/ciphers/", '_blank');
+            break;
+        case 'sort':
+            window.open("https://stephenmarri.github.io/js_projects/sortingVisualizer/", '_blank');
+            break;
+        case 'spaceInvaders':
+            window.open("https://stephenmarri.github.io/games/spaceInvaders/", '_blank');
+            break;
+        case 'tetris':
+            window.open("https://stephenmarri.github.io/games/tetris/", '_blank');
+            break;
+        case 'dadJokes':
+            window.open("https://stephenmarri.github.io/singletons/randomDadJokes/", '_blank');
+            break;
+        case 'mathFuntions':
+            window.open("https://stephenmarri.github.io/js_projects/mathFunctions/", '_blank');
+            break;
+
+        default:
+            break;
+    }
+}
