17db5c8dfa9e29d7a455c229b24a0f82539ca815
Marri Stephen <stephenmarri7@gmail.com>
2020-06-20 19:55:38 +0530

    projects page added with basic functionality

---
+++ portfolioPages/script_projects.js
@@ -0,0 +1,15 @@
+const sectionHeaders = document.querySelectorAll('#section__header')
+
+
+//######################################### Event Handlers
+sectionHeaders.forEach(x=> x.addEventListener('click',headinghandler))
+
+
+
+//######################################### Handler functions
+function headinghandler(){
+    this.classList.toggle('active')
+    const id = this.dataset.id
+    const tile = document.querySelector(`#tile${id}`)
+    tile.classList.toggle('active')
+}
