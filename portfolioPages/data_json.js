8889d3054636f8b188dceff1f637304c4f43cf22
Marri Stephen <stephenmarri7@gmail.com>
2020-06-21 19:25:16 +0530

    added json successfully

---
+++ portfolioPages/data_json.js
@@ -0,0 +1,150 @@
+let jsonData = {
+    section1: {
+        id:1,
+        title: "Vanilla JS Projects",
+        count: 2,
+        tiles: {
+            tile1:{
+                id: 1,
+                name: "Encryption Algorithms",
+                description:"A utility which converts or encrypts a given senctence or phrase into and encrypted version using the Ceaser's Cipher mechanism. It also converts using a secret key.",
+                imgSrc:"img_proj/ciphers.png",
+                link:"https://stephenmarri.github.io/js_projects/ciphers/"
+            },
+            tile2:{
+                id: 2,
+                name: "Jokes API",
+                description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, reiciendis!",
+                imgSrc:"img_proj/ciphers.png",
+                link:"https://stephenmarri.github.io/js_projects/ciphers/"
+            }
+        }
+    },
+    section2: {
+        id:2,
+        title: "JavaScript Games",
+        count: 2,
+        tiles: {
+            tile1:{
+                id: 1,
+                name: "Encryption Algorithms",
+                description:"A utility which converts or encrypts a given senctence or phrase into and encrypted version using the Ceaser's Cipher mechanism. It also converts using a secret key.",
+                imgSrc:"img_proj/ciphers.png",
+                link:"https://stephenmarri.github.io/js_projects/ciphers/"
+            },
+            tile2:{
+                id: 2,
+                name: "Jokes API",
+                description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, reiciendis!",
+                imgSrc:"img_proj/ciphers.png",
+                link:"https://stephenmarri.github.io/js_projects/ciphers/"
+            }
+        }
+    },
+    section3: {
+        id:3,
+        title: "JavaScript 30",
+        count: 2,
+        tiles: {
+            tile1:{
+                id: 1,
+                name: "Encryption Algorithms",
+                description:"A utility which converts or encrypts a given senctence or phrase into and encrypted version using the Ceaser's Cipher mechanism. It also converts using a secret key.",
+                imgSrc:"img_proj/ciphers.png",
+                link:"https://stephenmarri.github.io/js_projects/ciphers/"
+            },
+            tile2:{
+                id: 2,
+                name: "Jokes API",
+                description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, reiciendis!",
+                imgSrc:"img_proj/ciphers.png",
+                link:"https://stephenmarri.github.io/js_projects/ciphers/"
+            }
+        }
+    },
+    section4: {
+        id:4,
+        title: "Responsive HTML Pages",
+        count: 5,
+        tiles: {
+            tile1:{
+                id: 1,
+                name: "Encryption Algorithms",
+                description:"A utility which converts or encrypts a given senctence or phrase into and encrypted version using the Ceaser's Cipher mechanism. It also converts using a secret key.",
+                imgSrc:"img_proj/ciphers.png",
+                link:"https://stephenmarri.github.io/js_projects/ciphers/"
+            },
+            tile2:{
+                id: 2,
+                name: "Jokes API",
+                description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, reiciendis!",
+                imgSrc:"img_proj/ciphers.png",
+                link:"https://stephenmarri.github.io/js_projects/ciphers/"
+            },
+            tile3:{
+                id: 3,
+                name: "Jokes API",
+                description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, reiciendis!",
+                imgSrc:"img_proj/ciphers.png",
+                link:"https://stephenmarri.github.io/js_projects/ciphers/"
+            },
+            tile4:{
+                id: 4,
+                name: "Jokes API",
+                description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, reiciendis!",
+                imgSrc:"img_proj/ciphers.png",
+                link:"https://stephenmarri.github.io/js_projects/ciphers/"
+            },
+            tile5:{
+                id: 5,
+                name: "Jokes API",
+                description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, reiciendis!",
+                imgSrc:"img_proj/ciphers.png",
+                link:"https://stephenmarri.github.io/js_projects/ciphers/"
+            }
+        }
+    },
+    
+    section5: {
+        id:5,
+        title: "Project Euler",
+        count: 1,
+        tiles: {
+            tile1:{
+                id: 1,
+                name: "Project Euler",
+                description:"Solutions to the problems found on the popular website ProjectEuler.net. Solved the first 30 questions myself in Python and JavaScript. My Solutions can be accssed from the below link.",
+                imgSrc:"img_proj/ciphers.png",
+                link:"https://stephenmarri.github.io/js_projects/ciphers/"
+            }
+        }
+    },
+    section6: {
+        id:6,
+        title: "Single Utility Pages",
+        count: 3,
+        tiles: {
+            tile1: {
+                id: 1,
+                name: "CSS Search Box",
+                description:"A simple page with a interactive search box that expands on hovering.",
+                imgSrc:"img_proj/ciphers.png",
+                link:"https://stephenmarri.github.io/singletons/interactiveSearchBox/"
+            },
+            tile2:{
+                id: 2,
+                name: "Sample Sprite",
+                description:"A Simple sprite animation of a running girl",
+                imgSrc:"img_proj/ciphers.png",
+                link:"https://stephenmarri.github.io/singletons/sprites/index.html"
+            },
+            tile3:{
+                id: 3,
+                name: "Sample Sprite",
+                description:"A Simple sprite animation of a running girl",
+                imgSrc:"img_proj/ciphers.png",
+                link:"https://stephenmarri.github.io/singletons/sprites/index.html"
+            }
+        }
+    }
+}
--- portfolioPages/script_projects.js
+++ portfolioPages/script_projects.js
@@ -1,17 +1,102 @@
-const sectionHeaders = document.querySelectorAll('#section__header')
+let sectionHeaders;
+let listWrapper;
 
 
 //######################################### Event Handlers
-sectionHeaders.forEach(x=> x.addEventListener('click',headinghandler))
+window.addEventListener('DOMContentLoaded', init)
 
 
 
 //######################################### Handler functions
-function headinghandler(){
+function headinghandler() {
     this.classList.toggle('active')
     const id = this.dataset.id
     const tile = document.querySelector(`#tile${id}`)
     tile.classList.toggle('active')
     const symbol = this.querySelector('#symbol')
-    symbol.textContent= symbol.textContent == "+" ? "-" : "+" 
+    symbol.textContent = symbol.textContent == "+" ? "-" : "+"
+}
+
+
+//#################################### init
+function init() {
+    listWrapper = document.querySelector('.list--wrapper')
+
+    let styleToBeAdded=document.createElement('style')
+    let styleString=''
+    let styleStringMedia=''
+
+    const data = jsonData;
+    for (x in data) {
+        let id = data[x]["id"]
+        let title = data[x]["title"]
+        let tilesHtml=''
+        let height= parseInt(55 * parseInt(data[x]["count"]))
+        let heightMedia= parseInt(35 * parseInt(data[x]["count"]))
+
+        for(let i=1;i<=data[x]["count"];i++){
+            let name = data[x]["tiles"][`tile${i}`]["name"]
+            let description = data[x]["tiles"][`tile${i}`]["description"]
+            let imgSrc = data[x]["tiles"][`tile${i}`]["imgSrc"]
+            let link = data[x]["tiles"][`tile${i}`]["link"]
+            tilesHtml+= makeTile(name,description,imgSrc,link);
+        }
+
+        listWrapper.innerHTML += makeTileSection(id, title,tilesHtml);
+        styleString+=(addStyle(id, height))
+        styleStringMedia+=(addStyle(id, heightMedia))
+    }
+
+    styleToBeAdded.textContent=styleString    
+    styleToBeAdded.textContent += `@media only screen and (max-width: 600px) {${styleStringMedia}}`
+    document.head.append(styleToBeAdded)
+
+    //###################### initiate event listeners
+    sectionHeaders = document.querySelectorAll('#section__header')
+    sectionHeaders.forEach(x => x.addEventListener('click', headinghandler))
+}
+
+function makeTileSection(id, title, tilesHtml) {
+    let html = `
+    <div class="section" >                
+        <div id="section__header" data-id="${id}" >${title} 
+        <span id="symbol">+</span>
+        </div>                
+            <div class="tile--wrapper flex-col " id="tile${id}">     
+            
+            ${tilesHtml}
+            
+            </div>
+    </div>
+    `
+    return html
+}
+
+function makeTile(name, description, imgSrc, link) {
+    let html = `
+    <div class="tile flex-row"  >
+        <div id="tile_text" class="flex-col">
+            <span id="tText_head">${name}</span>
+            <span id="tText_desc">${description}</span>
+        </div>
+        <div id="tile_image" > 
+            <img src=${imgSrc} alt="">
+        </div>
+    </div>                    
+    `
+
+    return html
+}
+
+function addStyle(id,height){
+    const style = document.createElement('style');
+    let styleString =`
+    #tile${id}.active{
+        height: ${height}vh; 
+        margin: 1rem auto;
+    
+    }
+    `
+    style.textContent = styleString;
+    return styleString
 }
